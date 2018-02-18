fb117a58db12b640b81bf1364f679cc54212eb06
qwelyt <qwelyt@users.noreply.github.com>
2018-02-18 17:31:11 +0100

    make sidebar create menu

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,16 +6,47 @@ import Main from './Main'
 import Footer from './Footer'
 
 class App extends React.Component {
-    render() {
-      return (
-        <div className="container">
-          <Header look="head"/>
-          <Sidebar look="sidebar"/>
-          <Main look="main"/>
-          <Footer look="foot"/>
-        </div>
-      )
-    }
+  constructor(){
+    super();
+    this.state = {sidebarLook: 'sidebar'}
+  }
+
+  minimize(e){
+    let l = this.state.sidebarLook === 'sidebar' ? 'sidebar-mini' : 'sidebar'
+    this.setState({sidebarLook: l})
+  }
+  render() {
+    const menu = [
+      {
+        name: "Home"
+        ,link: "home"
+        , icon: ""
+      }
+      , {
+        name: "Wat"
+        ,link: "wat"
+        ,icon: ""
+      }
+      , {
+         name: "Other"
+         ,link: "other"
+         ,icon: ""
+       }
+       , {
+         name: "Someplace"
+         ,link: "potatos"
+         ,icon: ""
+       }
+     ]
+     return (
+       <div className="container">
+         <Header look="head"/>
+         <Sidebar look={this.state.sidebarLook} items={menu} minimize={this.minimize.bind(this)}/>
+         <Main look="main"/>
+         <Footer look="foot"/>
+       </div>
+     )
+  }
 }
 
 export default App
--- frontend/src/Sidebar.jsx
+++ frontend/src/Sidebar.jsx
@@ -6,6 +6,22 @@ class Sidebar extends React.Component {
     return (
       <div className={this.props.look}>
         Sidebar
+        <nav>
+          {this.props.items.map(i => {
+            return <NavItem key={i.name} item={i} />
+          })}
+        </nav>
+        <button onClick={this.props.minimize}>Minimize</button>
+      </div>
+    )
+  }
+}
+
+class NavItem extends React.Component {
+  render(){
+    return (
+      <div>
+        <a href={this.props.item.link}>{this.props.item.name}</a>
       </div>
     )
   }
